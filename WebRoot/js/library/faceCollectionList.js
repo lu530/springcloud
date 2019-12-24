0c3b5b1ad645a911a3ef0b5458703e7dda31af21
chenduoxin <chenduoxin@pcitech.com>
2019-12-24 14:48:18 +0800

    有效库残缺库调整字段

--- WebRoot/js/library/faceCollectionList.js
+++ WebRoot/js/library/faceCollectionList.js
@@ -1052,7 +1052,7 @@ function doSearch() {
             queryParams.SOURCE_TYPE = deviceType;
         }
 
-        queryParams.REGISTER_STATUS = $("#personBase .tag-item.active").attr("value");
+        queryParams.RLTZ_IS_NULL = $("#personBase .tag-item.active").attr("val");
         //检索案件录入
         /*if(isOpenSearchCause()){*/
         var searchParams = queryParams;
