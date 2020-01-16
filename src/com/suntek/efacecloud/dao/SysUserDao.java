454347b887bcb40a5b40d2182dfd22c8b2d46fb6
liaoweixiong <liaoweixiong@pcitech.com>
2020-01-16 16:40:23 +0800

    派出所用户判断优化

--- src/com/suntek/efacecloud/dao/SysUserDao.java
+++ src/com/suntek/efacecloud/dao/SysUserDao.java
@@ -13,9 +13,8 @@ public class SysUserDao {
 	private final JdbcTemplate jdbc = EAP.jdbc.getTemplate(Constants.APP_NAME);
 	
 	public List<Map<String, Object>> getUserListByUserCode(String userCode) {
-		String sql = "select user.USER_CODE from SYS_USER user "
-				+"inner join SYS_DEPT dept on user.DEPT_CODE = dept.DEPT_CODE "
-				+"where length(dept.CIVIL_CODE)>18 and user.USER_CODE = ?";
+		String sql = "select USER_CODE from SYS_USER "
+				+"where length(DEPT_CODE)>6 and USER_CODE = ?";
 		return jdbc.queryForList(sql,userCode);
 	}
 }
