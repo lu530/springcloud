67f1dcd477285575b020dec3f26b9078703f731f
xiehongtao <531026360@qq.com>
2020-01-17 11:22:57 +0800

    技战法异步跳转

--- WebRoot/js/technicalStation/personfrequentAccessRightList.js
+++ WebRoot/js/technicalStation/personfrequentAccessRightList.js
@@ -78,8 +78,12 @@ function initFrequentAccessList(){
 	}else{
 		UI.control.remoteCall('technicalTactics/frequencyAccess/query', queryParams, function(resp){
 			if(resp.IS_ASYNC == 1){
-				UI.util.alert("异步查询, " + resp.MESSAGE + " ,可到任务列表查询结果");
 				UI.util.hideLoadingPanel('currentPage');
+				UI.util.alert("异步查询, " + resp.MESSAGE + " , 即将打开任务列表");
+				setTimeout(() => {
+					var url = window.location.origin + '/portal/page/datadefenceMenu.html#tasklist';
+					window.open(url, '_blank');
+				}, 800);
 			}else{
 				dealWithListData(resp);
 			}
